21e0592a28bcccf4b069d2e8c8247981cd5dae52
someone <malu15258708@gmail.com>
2023-07-20 21:04:54 +0530

    JSON resume

---
+++ Task3resume.js
@@ -0,0 +1,12 @@
+let Resume=
+[{"name":"M.Malathi"},
+{"father's name":"V.Manoharan"},
+{"sex":"female"},
+{"date of birth":"15.02.1987"},
+{"nationaity":"indian"},
+{"Languages Known":"Tamil,English"},
+{"Address":"pasumainagar,Kumarapalayam main road,erode"},
+{"Educational Qualification":"B.sc.,B.Ed.,(Mathematics)"},
+{"Hobbies":"watching TV"}];
+
+console.log(Resume);
